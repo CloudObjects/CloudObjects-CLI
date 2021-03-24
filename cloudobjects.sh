55ec433c6ab0e57d577473b1e7a0025e3792fd6d
LukasRos <lr@lukasrosenstock.net>
2021-03-24 12:56:16 +0100

    TTY check when running in Docker

--- cloudobjects.sh
+++ cloudobjects.sh
@@ -1,1 +1,7 @@
-docker run --rm -ti -v $HOME:/root -v `pwd`:/workdir -w /workdir cloudobjects/cli $*
+#!/bin/bash
+if [ -t 0 ] && [ -t 1 ]; then
+    USE_TTY=t
+else
+    USE_TTY=
+fi
+docker run --rm -${USE_TTY}i -v $HOME:/root -v `pwd`:/workdir -w /workdir cloudobjects/cli $*
